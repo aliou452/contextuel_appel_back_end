aa7f1014578ffc4e2461538123f9a9f70bc21e9f
Aliou SY <sya@ept.sn>
2021-02-17 17:47:34 +0000

    Project initialization

---
+++ src/main/java/com/stgson/ContextualAppApplication.java
@@ -0,0 +1,13 @@
+package com.stgson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextualAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextualAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stgson/ContextualAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stgson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextualAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
