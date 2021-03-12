142e5afe6bfe20fb227e0aa3d906ec63efe027b6
Aliou SY <sya@ept.sn>
2021-03-12 17:56:13 +0000

    home api for test

--- src/main/java/com/stgson/ContextualAppApplication.java
+++ src/main/java/com/stgson/ContextualAppApplication.java
@@ -2,12 +2,21 @@ package com.stgson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("api/v1/home")
 public class ContextualAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContextualAppApplication.class, args);
 	}
 
+	@GetMapping
+	public String home(){
+		return "Hello everybody";
+	}
 }
